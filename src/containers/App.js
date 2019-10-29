4cf1f0a795680beac562970389f5c3038b46daff
Matthew Wayock <wayock@gmail.com>
2019-10-28 22:35:35 -0400

    refactor code and folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
   constructor (){
@@ -24,13 +24,13 @@ onSearchChange = (event) => {
 }
 
   render () {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0){
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
         <div className='tc'>
           <h1 className='f1'>Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
@@ -39,7 +39,6 @@ onSearchChange = (event) => {
           </Scroll>
         </div>
       );
-    }
   }
 }
 
