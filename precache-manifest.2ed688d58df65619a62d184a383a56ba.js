eb33323d0e479028c4d55e1d9388bdbda0fc7e47
Matthew Wayock <wayock@gmail.com>
2019-10-30 16:28:03 -0400

    Updates

---
+++ precache-manifest.2ed688d58df65619a62d184a383a56ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05d99ef6d82604c7a5208cd0cecd84f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "47a34c37d9b2d89866a8",
+    "url": "/robofriends/static/css/main.9fe01c81.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "47a34c37d9b2d89866a8",
+    "url": "/robofriends/static/js/main.f602d201.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
