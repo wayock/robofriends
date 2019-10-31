32efdc38f7f3b1256d5fa5912ddf0bd4b0742d83
Matthew Wayock <wayock@gmail.com>
2019-10-30 22:23:12 -0400

    redux actions, reducers, store, provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+const store = createStore(searchRobots)
+
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
